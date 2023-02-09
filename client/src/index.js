303c110e3b82d7db8d619a771749e864b2dcba57
Delfhin GOH <delfhingoh98@gmail.com>
2023-02-09 19:36:14 +0800

    Setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./index.css";
+
+const TestComponent = () => 
+{
+  return (
+    <h1>Test</h1>
+  );
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+root.render
+(
+  <TestComponent />
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
